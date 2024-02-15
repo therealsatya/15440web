0c986d878b2ae0f94a1e35b398ae16b6ef6ebed8
Mahadev Satyanarayanan <satya@cs.cmu.edu>
2024-02-15 16:09:33 -0500

    Added sed commands to change gitcdn.link to gitcdn.xyz to avoid painful slowness

--- publish_site.sh
+++ publish_site.sh
@@ -5,10 +5,25 @@ then
     USER="$1"
 fi
 
+# First create _site
+bundle exec jekyll build
+# Then fix the bad CDN link that causes slowness
+sed -e "s/gitcdn.link/gitcdn.xyz/" _site/index.html > _site/index-NEW.html
+mv _site/index-NEW.html _site/index.html
+sed -e "s/gitcdn.link/gitcdn.xyz/" _site/readings/index.html > _site/readings/index-NEW.html
+mv _site/readings/index-NEW.html _site/readings/index.html
+sed -e "s/gitcdn.link/gitcdn.xyz/" _site/slides/index.html > _site/slides/index-NEW.html
+mv _site/slides/index-NEW.html _site/slides/index.html
+sed -e "s/gitcdn.link/gitcdn.xyz/" _site/wellbeing/index.html > _site/wellbeing/index-NEW.html
+mv _site/wellbeing/index-NEW.html _site/wellbeing/index.html
+# Then copy to ~440 in AFS
+scp -r _site/* $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/440/www/
+
+# Beware: jekyll serve seems to copy over the sed fixes above
 #bundle exec jekyll serve
 
-bundle exec jekyll build
-scp -r _site/* $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/440/www/
+
+
 
 # The curl step below doesn't seem to work for me. After the above scp into
 # AFS, I have to manually go to the web page 
